eedd46328bccff258890fedba51ba84f2ef9c193
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-01-23 21:59:29 +0100

    PatchFridayModule

---
+++ modules/PatchFridayModule/PatchFridayModule.class.php
@@ -0,0 +1,13 @@
+<?php
+class PatchFridayModule extends BaseModule {
+    public $key = 'patchfriday';
+
+    public function get_data() {
+        $tmp = file_get_contents('https://patchfriday.com');
+        preg_match('/<div id="strip_title">(?\'title\'.*)<\/div>\s*<div id="strip_image"><img src="(?\'img\'[^"]*)"/', $tmp, $patchfriday);
+        error_log(print_r($patchfriday, true));
+        $json_obj->title = $patchfriday['title'];
+        $json_obj->src = 'https://patchfriday.com'.$patchfriday['src'];
+        return $json_obj;
+    }
+}
---
+++ modules/PatchFridayModule/PatchFridayModule.js
@@ -0,0 +1,18 @@
+function PatchFridayModule() {
+    this.loadStylesheet();
+    this.well = this.makeWell();
+    this.setInterval(6*60*60*1000); // 6 hours
+}
+
+PatchFridayModule.prototype = Object.create(BaseModule.prototype);
+PatchFridayModule.prototype.constructor = PatchFridayModule;
+PatchFridayModule.prototype.update = function(data) {
+    this.well.empty();
+    this.well.append(
+        $('<h2>').text(data.title),
+        $('<img>')
+            .attr.('src', data.src)
+    );
+}
+
+new PatchFridayModule();
