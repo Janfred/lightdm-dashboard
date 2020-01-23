541ffc506fc9b74a6709ffa6f66d6530c6875e83
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-01-23 20:47:53 +0100

    Use HTTPS instead of HTTP for stw-bremen.de

--- modules/GW2Module/GW2Module.class.php
+++ modules/GW2Module/GW2Module.class.php
@@ -3,6 +3,6 @@
 class GW2Module extends MensaModule {
     public function get_data() {
       $day = time() <= strtotime("14:30") ? strtotime("0:00") : strtotime("0:00 +1 day");
-      return parse_mensa("http://www.stw-bremen.de/de/essen-trinken/cafeteria-gw2", $day);
+      return parse_mensa("https://www.stw-bremen.de/de/essen-trinken/cafeteria-gw2", $day);
     }
 }
--- modules/MensaModule/MensaModule.class.php
+++ modules/MensaModule/MensaModule.class.php
@@ -2,9 +2,9 @@
 require_once("mensa-parser/mensa_parser.php");
 
 class MensaModule extends BaseModule {
-    
+
     public function get_data() {
         $day = time() <= strtotime("14:00") ? strtotime("0:00") : strtotime("0:00 +1 day");
-        return parse_mensa("http://www.stw-bremen.de/de/essen-trinken/uni-mensa", $day);
+        return parse_mensa("https://www.stw-bremen.de/de/essen-trinken/uni-mensa", $day);
     }
 }
