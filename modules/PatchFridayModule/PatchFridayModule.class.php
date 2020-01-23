84e027d5533268d1b9d7e70631bafdae43c23fe9
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-01-23 22:02:08 +0100

    fixup! PatchFridayModule

--- modules/PatchFridayModule/PatchFridayModule.class.php
+++ modules/PatchFridayModule/PatchFridayModule.class.php
@@ -7,7 +7,7 @@ class PatchFridayModule extends BaseModule {
         preg_match('/<div id="strip_title">(?\'title\'.*)<\/div>\s*<div id="strip_image"><img src="(?\'img\'[^"]*)"/', $tmp, $patchfriday);
         error_log(print_r($patchfriday, true));
         $json_obj->title = $patchfriday['title'];
-        $json_obj->src = 'https://patchfriday.com'.$patchfriday['src'];
+        $json_obj->src = 'https://patchfriday.com'.$patchfriday['img'];
         return $json_obj;
     }
 }
